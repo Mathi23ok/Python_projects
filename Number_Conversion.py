45901f4613e6f9a0a5e569f10a6ff8a949b3a1d9
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-04-27 21:53:23 +0530

    Completed Number Conversion System project: Added functionality to convert decimal to binary, octal, and hexadecimal with user input and exception handling.

--- Number_Conversion.py
+++ Number_Conversion.py
@@ -1,6 +1,6 @@
 print(":::::::::Number Conversion System:::::::::")
 def number_converter(a):
-  print("Decimal value : ",a);
+  print("Decimal value : ",a); 
   print("Binary value: ",bin(a)[2:]);
   print("Octal value : ",oct(a)[2:]);
   print("Hexadecimal Value : ",hex(a)[2:].upper())
